60991a49253fd8688b7037f43d8633a709983556
Iryna Kolomoiec <kolomoyka@gmail.com>
2022-11-12 16:23:06 +0100

    add

---
+++ src/js/02-timer.js
@@ -0,0 +1,6 @@
+import flatpickr from "flatpickr";
+import "flatpickr/dist/flatpickr.min.css";
+
+const btnStart = document.querySelector('button[data-start]');
+console.log(btnStart)
+btnStart.disabled = false;
