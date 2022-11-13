7f6a310b4725b3aa9fae7ad6123885dc4a71e105
Iryna Kolomoiec <kolomoyka@gmail.com>
2022-11-14 00:57:34 +0100

    Update 03-promises.js

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,4 @@
 import Notiflix from 'notiflix';
-//const delay = document.querySelector('input[name="delay"]');
-//const step = document.querySelector('input[name="step"]');
-//const amount = document.querySelector('input[name="amount"]');
-//const btnCreatePromise = document.querySelector('button[type="submit"]');
 
 function createPromise(position, delay) {
   const promise = new Promise ((resolve, reject) => {
@@ -28,7 +24,7 @@ function onSubmit(e) {
   const amount = Number(e.currentTarget.amount.value);
   const step = Number(e.currentTarget.step.value);
   for (let i=1; i <= amount; i += 1) {
-    createPromise(1, delay)
+    createPromise(i, delay)
   .then(({ position, delay }) => {
     console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
     //Notiflix.Notify.success(`Fulfill promise ${position} in $[delay] ms`)
@@ -37,6 +33,6 @@ function onSubmit(e) {
     console.log(`❌ Rejected promise ${position} in ${delay}ms`);
     //Notiflix.Notify.failure(`Rejected promise ${position} in $[delay] ms`)
   });
-  delay += step
+  delay += step;
 }
 }
